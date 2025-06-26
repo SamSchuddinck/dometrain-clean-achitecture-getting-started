9a9aec8ad3e3b288a4a31a604a9e4e5bc93dacc0
Sam Schuddinck <sam.schuddinck@euri.com>
2025-06-26 12:30:13 +0200

    feat: Implementing Strongly Typed Enums

---
+++ GymManagement.Domain/Subscriptions/SubscriptionType.cs
@@ -0,0 +1,18 @@
+using System;
+using Ardalis.SmartEnum;
+
+namespace GymManagement.Domain.Subscriptions;
+
+public class SubscriptionType: SmartEnum<SubscriptionType>
+{
+    public static readonly SubscriptionType Free = new(0, nameof(Free));
+    public static readonly SubscriptionType Starter = new(1, nameof(Starter));
+    public static readonly SubscriptionType Pro = new(2, nameof(Pro));
+
+    private SubscriptionType(int value, string name) : base(name, value)
+    {
+    }
+}
+{
+
+}
